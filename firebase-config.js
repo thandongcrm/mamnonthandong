9322b3aa91b3d58b6276a621343e4fbb207dd3a7
thandongcrm <thandongsieunhi@gmail.com>
2026-08-07 10:31:06 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,13 @@
-// Cấu hình Firebase chuẩn cho phiên bản Compat (Dán đè toàn bộ nội dung vào file firebase-config.js)
 const firebaseConfig = {
-    apiKey: "ĐIỀN_API_KEY_CỦA_BẠN_VÀO_ĐÂY",
-    authDomain: "ĐIỀN_AUTH_DOMAIN_CỦA_BẠN_VÀO_ĐÂY",
+    apiKey: "Dán_API_Key_của_bạn_vào_đây",
+    authDomain: "Dán_Auth_Domain_của_bạn_vào_đây",
     projectId: "crmmamnon",
-    storageBucket: "ĐIỀN_STORAGE_BUCKET_CỦA_BẠN_VÀO_ĐÂY",
-    messagingSenderId: "ĐIỀN_MESSAGING_SENDER_ID_CỦA_BẠN_VÀO_ĐÂY",
-    appId: "ĐIỀN_APP_ID_CỦA_BẠN_VÀO_ĐÂY"
+    storageBucket: "Dán_Storage_Bucket_của_bạn_vào_đây",
+    messagingSenderId: "Dán_Messaging_Sender_Id_vào_đây",
+    appId: "Dán_App_Id_của_bạn_vào_đây"
 };
 
-// Khởi tạo Firebase
+// Khởi tạo Firebase Compat
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
