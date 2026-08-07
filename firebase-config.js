6208b9bf7a5560be6a3d5ae3500f74e809e7bb31
thandongcrm <thandongsieunhi@gmail.com>
2026-08-07 09:08:43 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyBE3PkAPgI30UblV8e6B12RdXU-gx9gFHI",
+    authDomain: "crmmamnon.firebaseapp.com",
+    projectId: "crmmamnon",
+    storageBucket: "crmmamnon.firebasestorage.app",
+    messagingSenderId: "759420339515",
+    appId: "1:759420339515:web:66900a20d0eb59bc73a31e"
+};
+
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
